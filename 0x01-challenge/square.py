4c1474cec77e583889aa5ed58f1e57c694b01a45
Celse13 <117368635+Celse13@users.noreply.github.com>
2023-12-18 22:43:45 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""This is a structure of my square."""
+"""The Square Class"""
 
 
-class square():
-    """The class of the square."""
-    
+class Square:
+    """Square Class"""
     width = 0
     height = 0
 
-    
-    def __init__(self, *args, **kwargs):
-        """Initialize the square."""
+    def __init__(self, **kwargs):
+        """Initialize Square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """Area of the square"""
+        return self.width * self.height
 
-    def perimeter_of_my_square(self):
-        """Calculate the perimeter of the square"""
-        return (self.width * 2) + (self.height * 2)
+    def permiter_of_my_square(self):
+        """Perimeter of square"""
+        return (self.width + self.height) * 2
 
     def __str__(self):
-        """String representation of the square."""
-        return "{}/{}".format(self.width, self.height)
+        """String rep of Square"""
+        return f"{self.width}/{self.height}"
+
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    """Main entry point"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
